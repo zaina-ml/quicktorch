8c599292ee1e185b582e08fe5dbe3d69821c4560
zaina-ml <zainaboobacker33@gmail.com>
2025-04-14 21:33:21 -0400

    Update quicktorch.py
    Removed Permute, Unsqueeze. Added Confusion Matrix, Diagnostics and improved performance using batches.

--- quicktorch.py
+++ quicktorch.py
@@ -4,89 +4,178 @@ from torch import nn
 import matplotlib.pyplot as plt
 import random
 
+import torchmetrics
+
+from torchmetrics import ConfusionMatrix
+from torchmetrics.classification import Accuracy, Precision, Recall, F1Score, AUROC    
+
+
+import mlxtend
+from mlxtend.plotting import plot_confusion_matrix
 
 
 """
-A simple PyTorch library with 10 functions made to make the PyTorch workflow easier
+A simple PyTorch library with functions made to make the PyTorch workflow easier
 """
+
+# --- Performs Tests Such as Accuracy, Precision, Recall, F1Core and AUROC --- #
+
+def run_diagnostic(model: torch.nn.Module, dataset: torch.utils.data.Dataset, task: str, device: str, BATCH_SIZE=32) -> tuple:
+    """
+    Performs Tests Such as Accuracy, Precision, Recall, F1Score and AUROC Over a Given Dataset
+    
+    Args: 
+    model (torch.nn.Module): PyTorch Model to be tested
+    dataset (torch.utils.data.Dataset): PyTorch dataset where samples will be extracted
+    task (str): either "multiclass" or "binary" (parameter used by torchmetrics)
+    device (str): current device that model is on
+    BATCH_SIZE (int): batch sizes of dataset when fitted to model
+  
+    Returns:
+    Tuple containing Accuracy, Precision, Recall, F1Scorea and AUROC
+    """
+    if task != "multiclass" and task != "binary":
+        raise Exception("Task parameter must either be (multiclass, binary)")
+
+    accuracy = Accuracy(task=task, num_classes=len(dataset.classes)).to(device)
+    precision = Precision(task=task, num_classes=len(dataset.classes)).to(device)
+    recall = Recall(task=task, num_classes=len(dataset.classes)).to(device)
+    f1_score = F1Score(task=task, num_classes=len(dataset.classes)).to(device)
+    auroc = AUROC(task=task, num_classes=len(dataset.classes)).to(device)
+
+    model.eval()
+  
+    with torch.inference_mode():  
+      for X, y in torch.utils.data.DataLoader(dataset=dataset, batch_size=BATCH_SIZE):
+        X, y = X.to(device), y.to(device)
+
+        y_hat = model(X)
+        y_labels = torch.argmax(torch.softmax(y_hat, dim=1), dim=1)
+
+        accuracy.update(y_labels, y)
+        precision.update(y_labels, y)
+        recall.update(y_labels, y)
+        f1_score.update(y_labels, y)
+        auroc.update(y_hat, y)
+        
+    print(f"Accuracy:  {accuracy.compute().item():.4f}")
+    print(f"Precision: {precision.compute().item():.4f}")
+    print(f"Recall:    {recall.compute().item():.4f}")
+    print(f"F1 Score:  {f1_score.compute().item():.4f}")
+    print(f"AUROC:     {auroc.compute().item():.4f}")
+
+    return accuracy.compute().item(), precision.compute().item(), recall.compute().item(), f1_score.compute().item(), auroc.compute().item()
+
+# --- Plots Confusion Matrix --- #
+
+def confusion_matrix(model: torch.nn.Module, dataset: torch.utils.data.Dataset, task: str, device: str):
+    """
+    Plots Confusion Matrix on a Model with a given Dataset
+    
+    Args: 
+    model (torch.nn.Module): PyTorch Model to be tested
+    dataset (torch.utils.data.Dataset): PyTorch dataset where samples will be extracted
+    task (str): either "multiclass" or "binary" (parameter used by torchmetrics)
+    device (str): current device that model is on
+  
+    Returns:
+    Confusion Matrix in Matplotlib
+    """
+
+    if task != "multiclass" and task != "binary":
+        raise Exception("Task parameter must either be (multiclass, binary)")
+
+    
+    y_true = dataset.targets
+    y_hat = make_predictions(model=model, dataset=dataset, device=device)
+    y_probs = torch.softmax(y_hat, dim=1)
+
+    confusion_matrix = ConfusionMatrix(task=task, num_classes=len(dataset.classes)).to(device)
+    confusion_matrix_tensor = confusion_matrix(preds=y_probs,
+                                               target=torch.tensor(dataset.targets, device=device))
+
+    figure, axis = plot_confusion_matrix(
+        conf_mat=confusion_matrix_tensor.cpu().numpy(),
+        class_names=dataset.classes,
+        figsize=(10, 7)
+    )
+
+# --- Converts Logits to Probabilities --- #
+
+def convert_to_probabilities(predictions: torch.Tensor, task: str):
+    """
+    Converts Given Logits to Probabilities
+
+    Args:
+    predictions(torch.Tensor): logits given to be converted to prediction probabilities
+    task (str): either multiclass or binary (used to determine usage of sigmond or softmax)
+
+    Returns:
+    A Torch Tensor Of Probabilities
+
+    """
+    if task != "multiclass" and task != "binary":
+        raise Exception("Task parameter must either be (multiclass, binary)")
+
+    if task == "multiclass":
+        return torch.softmax(predictions, dim=1)
+    elif task == "binary":
+        return torch.sigmoid(predictions, dim=1)
+
 
 # --- Generates Random Samples --- #
 
-def generate_samples(dataset, n_samples: int) -> list:
+def generate_samples(dataset: torch.utils.data.Dataset, n_samples: int) -> torch.utils.data.Dataset:
     """
     Generates a list of samples from the given dataset randomly
     
     Args: 
-    dataset (Any): dataset where samples will be extracted (must be unpackable)
+    dataset (torch.utils.data.Dataset): PyTorch dataset where samples will be extracted
     n_samples (int): amount of samples to be extracted from dataset
   
     Returns:
-    A list consisting of random samples extracted from the dataset with a length of n_samples along with their corresponding label (can be unpacked)
+    A new PyTorch dataset of the random samples
     """
     
-    samples = []
-  
-    for sample, label in random.sample(list(dataset), k=n_samples):
-      samples.append((sample, label))
-  
-    return samples
+    random_samples = random.sample(list(dataset), k=n_samples)
 
-# --- Function For Unsqueezing a Dataset --- #
+    class RandomSampleDataset(torch.utils.data.Dataset):
+        def __init__(self, random_samples=random_samples, classes=dataset.classes, class_to_idx=dataset.class_to_idx):
+            self.random_samples = random_samples
 
-def unsqueeze_dataset(dataset, dim) -> list:
+            self.classes, self.class_to_idx = classes, class_to_idx
+            self.targets = self.get_targets()
+        
+        def get_targets(self):
+            targets = []
+
+            for X, y in random_samples:
+                targets.append(y)
+
+            return torch.tensor(targets)
+        
+        def __getitem__(self, idx: int):
+            return (random_samples[idx][0], random_samples[idx][1])
+
+        def __len__(self):
+            return len(self.random_samples)
     
-    """
-    Unsqueezes all samples on a given dataset on a given dimension (you should never be overwriting the original dataset)
+    random_sample_dataset = RandomSampleDataset()
 
-    Args:
-    dataset (Any): dataset being unsqueezed (only samples will be unsqeezed and must be unpackable)
-    dim (int): the target dimension of a certain dataset sample being unsqueezed
+    return random_sample_dataset
 
-    Returns:
-    An unsqueezed dataset
-    """
-    
-    unsqueezed_dataset = []
-    
-    for sample, label in dataset:
-        unsqueezed_sample = torch.unsqueeze(sample, dim=dim)
 
-        unsqueezed_dataset.append((unsqueezed_sample, label))
+# --- Plots A Set Of Images --- #
 
-    return unsqueezed_dataset
-
-# --- Function For Permuting A Dataset --- #
-
-def permute_dataset(dataset, *argv) -> list:
-    """
-    Permutes all samples on a given dataset given dimension indexes (you should never be overwriting the original dataset)
-
-    Args:
-    dataset (Any): the dataset being permuted (only samples will be permuted)
-    *argv (ints): the dimension indexes that the samples will be permuted on
-
-    Returns:
-    A permuted dataset
-    """
-    permuted_dataset = []
-
-    for sample, label in dataset:
-        permuted_dataset.append((sample.permute(argv), label))
-    
-    return permuted_dataset
-
-# --- Function For Plotting Images --- #
-
-def plot_images(dataset, rows: int, columns: int, figsize: tuple, classes=None, cmap=None, title=True, fontsize=10) -> None:
+def plot_images(dataset: torch.utils.data.Dataset, rows: int, columns: int, figsize: tuple, cmap=None, title=True, fontsize=10) -> None:
     """
     Plots a certain amount of images from the given dataset
   
     Args:
-    dataset (Any): dataset in which images are stored (must be unpackable, if length does not equal rows * columns, the first rows * columns images of the dataset will be plotted)
+    dataset (torch.utils.data.Dataset): the PyTorch dataset in which images are stored (must be unpackable, if length does not equal rows * columns, the first rows * columns images of the dataset will be plotted)
     rows (int): how many rows of images the figure will consist of 
     columns (int): how many columns of images the figure will consist of
     figsize (tuple): what the resulting figure size of the figure will be
-    classes (dict): what the function will use when plotting image labels
     cmap (str, optional): what the color map of the plotted images will be
     title (bool, optional): whether or not a title will be displayed for each image (defaults to True)
     fontsize (int, optional): font size of the title (defaults to 10)
@@ -103,7 +192,7 @@ def plot_images(dataset, rows: int, columns: int, figsize: tuple, classes=None, 
         plt.subplot(rows, columns, i+1)
   
         if title:
-            plt.title(classes[label], fontsize=fontsize)
+            plt.title(dataset.classes[label], fontsize=fontsize)
 
         plt.imshow(image.squeeze(), cmap=cmap)  
         plt.axis(False)
@@ -112,16 +201,16 @@ def plot_images(dataset, rows: int, columns: int, figsize: tuple, classes=None, 
 
 # --- Plotting Images And Their Predictions --- #
 
-def plot_image_predictions(predictions, dataset, rows: int, columns: int, figsize: tuple, classes: dict, fontsize=10, cmap=None) -> None:
-    """ Plots the image being predicted on and the models prediction on it 
+def plot_image_predictions(predictions: torch.Tensor, dataset: torch.utils.data.Dataset, rows: int, columns: int, figsize: tuple, fontsize=10, cmap=None) -> None:
+    """ 
+    Plots the image being predicted on and the models prediction on it 
   
     Args:
-    predictions (Any): the model's predictions on the dataset (must be in probability form)
-    dataset (Any): The dataset the model predicted on
+    predictions (torch.Tensor): the model's predictions on the dataset (must be in probability form)
+    dataset (torch.utils.data.Dataset): The PyTorch dataset the model predicted on
     rows (int): how many rows of images the figure will consist of 
     columns (int): how many columns of images the figure will consist of 
     figsize (tuple): the resulting figure size of the plot
-    classes (dict): the classes of the dataset
     fontsize (int, optional): the fontsize of the resulting plot (default is 10)
     cmap (str, optional): the colormap of the resulting images
   
@@ -137,11 +226,15 @@ def plot_image_predictions(predictions, dataset, rows: int, columns: int, figsiz
         image, label = dataset[i]
         prediction = predictions[i]
   
-        title = f"Predicted: {classes[prediction.argmax()]}: {prediction.max(): .2f} | Truth: {classes[label]}"
-        color = "green" if classes[prediction.argmax()] == classes[label] else "red"
+        title = f"Predicted: {dataset.classes[prediction.argmax()]}: {prediction.max(): .2f} | Truth: {dataset.classes[label]}"
+        color = "green" if dataset.classes[prediction.argmax()] == dataset.classes[label] else "red"
   
         plt.title(title, fontsize=fontsize, c=color)
-        plt.imshow(image.squeeze(), cmap=cmap)
+
+        if len(torch.squeeze(image).shape) == 2:
+            plt.imshow(torch.squeeze(image), cmap=cmap)
+        elif len(torch.squeeze(image).shape) == 3:
+            plt.imshow(torch.squeeze(image).permute(1, 2, 0))
         
         plt.axis(False)
     plt.show();
@@ -185,69 +278,17 @@ def plot_results(results: dict, figsize=(15, 7)) -> None:
     plt.show();
 
 
-# --- Accuracy Evaluation Function --- #
-
-def eval_accuracy(model: torch.nn.Module, dataset, device: str) -> float:
-    """
-    Evaluates a model's accuracy on a given dataset
-
-    Args: 
-    model (torch.nn.Module): the model being evaluated
-    dataset (Any): an unpackable iterable (ex. [(sample, label), (sample, label)]) that the model will be evaluated on
-    device (str): the target device for the model
-
-    Returns:
-    accuracy (float): the accuracy of the model on the dataset
-    """
-
-    correct  = 0
-    
-    with torch.inference_mode():
-        model.eval()
-
-        for sample, label in dataset:
-            y_hat = model(sample.to(device)) 
-
-            if y_hat.argmax() == label:
-                correct += 1
-
-    return correct / len(dataset)
-
-# --- Plots Linear Regression Predictions --- #
-
-def plot_linear_predictions(X: torch.Tensor, y: torch.Tensor, predictions=None, colors=['r','g'], figsize=(10, 7)) -> None:
-    """
-    Plots the linear predictions of a certain model
-  
-    Args:
-    X (torch.Tensor): the data inputed through the model
-    y (torch.Tensor): the labels of the data given
-    predictions (torch.Tensor, optional): The predictions of the model
-    colors (list, optional): The colors of each of scatter plots (defaults to [r, g])
-    figsize (tuple, optional): The figure size of the returned plot
-  
-    Returns:
-    A Matplotlib scatter plot consisting of the data, labels and the models predictions
-    """
-    plt.figure(figsize=figsize)
-  
-    plt.scatter(X, y, c=colors[0], s=4, label="Data")
-    
-    if predictions is not None:
-        plt.scatter(X, predictions, c=colors[1], s=4, label="Predictions")
-  
-    plt.legend(prop={"size": 14})
-
 # --- Function For Making Predictions --- #
 
-def make_predictions(model: torch.nn.Module, dataset, device: str):
+def make_predictions(model: torch.nn.Module, dataset: torch.utils.data.Dataset, device: str, BATCH_SIZE=32) -> torch.Tensor:
     """
     Makes predictions using the given model on a given dataset
   
     Args: 
     model (torch.nn.Module): the model predicting on the given dataset
-    dataset (Any): the dataset in which the model will be predicting on
+    dataset (torch.utils.data.Dataset): the dataset in which the model will be predicting on 
     device (str): specifies what device the data should be on
+    BATCH_SIZE (int, optional): specifies the batch size of data when predicted on (def. 32)
   
     Returns:
     A tensor of predictions from the model (in raw logit form)
@@ -255,27 +296,27 @@ def make_predictions(model: torch.nn.Module, dataset, device: str):
     model.eval()
   
     with torch.inference_mode():
-      predictions = []
+      predictions = torch.tensor([], device=device)
       
-      for X, y in dataset:
+      for X, y in torch.utils.data.DataLoader(dataset=dataset, batch_size=BATCH_SIZE):
         X = X.to(device)
         
         prediction = model(X)
-  
-        predictions.append(prediction)
 
-    return torch.cat(predictions)
+        predictions = torch.cat((predictions, prediction), dim=0)
+
+    return predictions
 
 
 # --- Train Function --- #
 
-def train(epochs: int, 
-          model: torch.nn.Module, 
-          loss_fn: torch.nn.Module, 
-          optimizer: torch.optim.Optimizer, 
-          train_dataloader: torch.utils.data.DataLoader, 
-          test_dataloader: torch.utils.data.DataLoader, 
-          device:str) -> dict:
+def train_model(epochs: int, 
+                model: torch.nn.Module, 
+                loss_fn: torch.nn.Module, 
+                optimizer: torch.optim.Optimizer, 
+                train_dataloader: torch.utils.data.DataLoader, 
+                test_dataloader: torch.utils.data.DataLoader, 
+                device:str) -> dict:
     """
     Performs a training &  test loop over an iterable torch dataloader with the given model
 
